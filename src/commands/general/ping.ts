a5b4f19046aa29b9f5a28d82ddec1e42929b85fa
bordoga_bordoga <kolinjahin@gmail.com>
2022-11-13 17:05:31 +0100

    UV1

--- src/commands/general/ping.ts
+++ src/commands/general/ping.ts
@@ -2,18 +2,21 @@ import { Command } from "@sapphire/framework";
 import { Message } from "discord.js";
 
 export class PingCommand extends Command {
-    public constructor(context: Command.Context, options: Command.Options) {
-        super(context, {
-            ...options,      
-            name: "ping",
-            aliases: ["latency"],
-            description:
-                "See the Latency in milliseconds between the Discord API and Server",
-            fullCategory: ["general"],
-        });
-    }
+   public constructor(context: Command.Context, options: Command.Options) {
+      super(context, {
+         ...options,
+         name: "ping",
+         aliases: ["latency"],
+         description:
+            "See the Latency in milliseconds between the Discord API and Server",
+         fullCategory: ["general"],
+      });
+   }
 
-    public messageRun(message: Message) {
-        return message.channel.send(`Pong! \`${this.container.client.ws.ping}ms\``);
-    }
+   public messageRun(message: Message) {
+      console.log("hmmmmm");
+      return message.channel.send(
+         `Pong! \`${this.container.client.ws.ping}ms\``
+      );
+   }
 }
--- src/listeners/client/ready.ts
+++ src/listeners/client/ready.ts
@@ -2,17 +2,17 @@ import { Listener } from "@sapphire/framework";
 import { Client } from "discord.js";
 
 export class ReadyListener extends Listener {
-    public constructor(context: Listener.Context, options: Listener.Options) {
-        super(context, {
-            ...options,
-            once: true,
-        });
-    }
+   public constructor(context: Listener.Context, options: Listener.Options) {
+      super(context, {
+         ...options,
+         once: true,
+      });
+   }
 
-    public run(client: Client) {
-        const { tag, id } = client.user!;
-        return this.container.logger.info(
-            `Successfully logged in as ${tag} (${id})`
-        );
-    }
+   public run(client: Client) {
+      const { tag, id } = client.user!;
+      return this.container.logger.info(
+         `Successfully logged in as ${tag} (${id})`
+      );
+   }
 }
